550049631b531b180069a26f57372a55cdbd6bb0
catoni0 <mr.vladimir1337@gmail.com>
2025-03-01 16:31:57 +0200

    System_model

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,10 @@ class Mode(Enum):
 # Settings
 DEFAULT_HOST = "http://localhost:11434"
 
-DEFAULT_MODEL = "deepseek-r1:7b"
+DEFAULT_MODEL = "deepseek-r1:14b"
 CODE_MODEL = "deepseek-coder-v2:16b"
 SHELL_MODEL = "deepseek-coder-v2:16b"
+SYSTEM_MODEL = "deepseek-r1:7b"
 HELPER_MODEL = "deepseek-r1:1.5b"
 VISION_MODEL = "llama3.2-vision:11b"
 EMBEDDING_MODEL = "nomic-embed-text:latest"
@@ -25,7 +26,7 @@ MODE_CONFIGS = {
     Mode.DEFAULT: {"model": DEFAULT_MODEL, "temp": 0.4, "prompt": "", "stream": True},
     Mode.CODE:    {"model": CODE_MODEL, "temp": 0.5, "prompt": CODE, "stream": True},
     Mode.SHELL:   {"model": SHELL_MODEL, "temp": 0.4, "prompt": SHELL, "stream": True},
-    Mode.SYSTEM:  {"model": SHELL_MODEL, "temp": 0.5, "prompt": SYSTEM, "stream": True},
+    Mode.SYSTEM:  {"model": SYSTEM_MODEL, "temp": 0.5, "prompt": SYSTEM, "stream": True},
     Mode.HELPER:  {"model": HELPER_MODEL, "temp": 0.5, "prompt": "", "stream": False},
     Mode.VISION:  {"model": VISION_MODEL, "temp": 0.6, "prompt": "", "stream": False},
 
